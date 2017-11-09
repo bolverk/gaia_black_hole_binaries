d4d71aee8d74666b0f1412caa6cef18ffb861f8f
bolverk <almog.yalin@gmail.com>
2017-11-09 07:56:45 -0500

    add mind map

---
+++ Gaia Black hole binaries.mm
@@ -0,0 +1,20 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1510230203070" ID="ID_1425632117" MODIFIED="1510230284459" STYLE="bubble" TEXT="Gaia Black hole binaries">
+<node CREATED="1510230268184" ID="ID_1501031073" MODIFIED="1510230284459" POSITION="right" TEXT="Previous works">
+<node CREATED="1510230304040" ID="ID_676828124" LINK="http://adsabs.harvard.edu/cgi-bin/bib_query?arXiv:1710.09839" MODIFIED="1510230313419" TEXT="Yamaguchi et al"/>
+</node>
+<node CREATED="1510230657450" ID="ID_295170164" MODIFIED="1510230698444" POSITION="left" TEXT="Theoretical modelling">
+<node CREATED="1510230700186" ID="ID_846402411" MODIFIED="1510230705290" TEXT="Uncertainty in mass">
+<node CREATED="1510230706635" ID="ID_1214732964" MODIFIED="1510230907650" TEXT="A priori, between 0.01 and 100 solar masses"/>
+</node>
+<node CREATED="1510230949445" ID="ID_663960226" MODIFIED="1510230959418" TEXT="Uncertainty in semi major axis">
+<node CREATED="1510230965322" ID="ID_1982083513" MODIFIED="1510231121221" TEXT="Greater than Roche limit (otherwise it will emit in the x rays)"/>
+<node CREATED="1510231707281" ID="ID_312875170" MODIFIED="1510231742453" TEXT="Upper bound given by tidal forces from the galaxy"/>
+</node>
+<node CREATED="1510231810455" ID="ID_652980626" MODIFIED="1510231816087" TEXT="Galactic extinction"/>
+<node CREATED="1510231820787" ID="ID_1415881942" MODIFIED="1510231822177" TEXT="Kicks"/>
+<node CREATED="1510231825490" ID="ID_175029529" MODIFIED="1510231915988" TEXT="Nyquist effect for rapidly rotating binaries"/>
+</node>
+</node>
+</map>
